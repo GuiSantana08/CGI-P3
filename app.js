f8b756fb72311134dc1fd54499521d8581988713
DiogoARSantos <113592569+DiogoARSantos@users.noreply.github.com>
2022-12-13 02:54:28 +0000

    GUI feito

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import { lookAt, perspective, vec3 } from "../../libs/MV.js";
-import { GUI } from '../../libs/dat.gui.module.js';
+import { buildProgramFromSources, loadShadersFromURLS, setupWebGL } from '../../libs/utils.js';
+import {flatten, lookAt, perspective, vec3, normalMatrix } from "../../libs/MV.js";
+
+import * as dat from '../../libs/dat.gui.module.js';
 
 import * as SPHERE from '../../libs/objects/sphere.js';
 import * as CUBE from '../../libs/objects/cube.js';
@@ -7,118 +9,10 @@ import * as TORUS from '../../libs/objects/torus.js';
 import * as CYLINDER from '../../libs/objects/cylinder.js';
 import * as BUNNY from '../../libs/objects/bunny.js';
 
-let options = {'backface culling':false, 'depth test':false}
-let camera = {fovy:0, near:0, far:0}
-let eye = {x:0, y:0, z:0}
-let at = {x:0, y:0, z:0}
-let up = {x:0, y:0, z:0}
-let position1 = {x:0, y:0, z:0, w:0} 
-let intensities1 = {ambient:vec3(0,0,0), diffuse:vec3(0,0,0), specular:vec3(0,0,0)}
-let aixs1 = {x:0, y:0, z:0, aperture:0, cutoff:0}
-let position2 = {x:0, y:0, z:0, w:0} 
-let intensities2 = {ambient:vec3(0,0,0), diffuse:vec3(0,0,0), specular:vec3(0,0,0)}
-let aixs2 = {x:0, y:0, z:0, aperture:0, cutoff:0}
-let position3 = {x:0, y:0, z:0, w:0} 
-let intensities3 = {ambient:vec3(0,0,0), diffuse:vec3(0,0,0), specular:vec3(0,0,0)}
-let aixs3 = {x:0, y:0, z:0, aperture:0, cutoff:0}
-
-let material = {Ka:vec3(0,0,0), Kd:vec3(0,0,0), Ks:vec3(0,0,0), shininess:0}
-
-const gui = new GUI();
-const optionsFolder = gui.addFolder('options');
-const cameraFolder = gui.addFolder('camera');
-const lightsFolder = gui.addFolder('lights');
-const materialFolder = gui.addFolder('material');
-
-//options
-optionsFolder.add(options, 'backface culling',false,true);
-optionsFolder.add(options, 'depth test',0,100);
-
-//camera
-cameraFolder.add(camera, 'fovy', 0, 360);
-cameraFolder.add(camera, 'near' , 0, 360);
-cameraFolder.add(camera, 'far' , 0, 360);
-const eyeFolder = cameraFolder.addFolder('eye');
-const atFolder= cameraFolder.addFolder('at');
-const upFolder= cameraFolder.addFolder('up');
-//eye
-eyeFolder.add(eye,'x');
-eyeFolder.add(eye,'y');
-eyeFolder.add(eye,'z');
-//at
-atFolder.add(at,'x');
-atFolder.add(at,'y');
-atFolder.add(at,'z');
-//up
-upFolder.add(up,'x');
-upFolder.add(up,'y');
-upFolder.add(up,'z');
-
-
-//lights
-//Light1
-const light1 = lightsFolder.addFolder('Light1');
-const pos1 = light1.addFolder('position');
-pos1.add(position1,'x');
-pos1.add(position1,'y');
-pos1.add(position1,'z');
-pos1.add(position1,'w');
-const intens1 = light1.addFolder('intensities');
-intens1.addColor(intensities1,'ambient');
-intens1.addColor(intensities1,'diffuse');
-intens1.addColor(intensities1,'specular');
-const ax1 = light1.addFolder('axis');
-ax1.add(aixs1,'x');
-ax1.add(aixs1,'y');
-ax1.add(aixs1,'z');
-ax1.add(aixs1,'aperture',0,100);
-ax1.add(aixs1,'cutoff',0,100);
-
-//Light2
-const light2 = lightsFolder.addFolder('Light2');
-const pos2 = light2.addFolder('position');
-pos2.add(position2,'x');
-pos2.add(position2,'y');
-pos2.add(position2,'z');
-pos2.add(position2,'w');
-const intens2 = light2.addFolder('intensities');
-intens2.addColor(intensities2,'ambient');
-intens2.addColor(intensities2,'diffuse');
-intens2.addColor(intensities2,'specular');
-const ax2 = light2.addFolder('axis');
-ax2.add(aixs2,'x');
-ax2.add(aixs2,'y');
-ax2.add(aixs2,'z');
-ax2.add(aixs2,'aperture',0,100);
-ax2.add(aixs2,'cutoff',0,100);
-
-//Light3
-const light3 = lightsFolder.addFolder('Light3');
-const pos3 = light3.addFolder('position');
-pos3.add(position3,'x');
-pos3.add(position3,'y');
-pos3.add(position3,'z');
-pos3.add(position3,'w');
-const intens3 = light3.addFolder('intensities');
-intens3.addColor(intensities3,'ambient');
-intens3.addColor(intensities3,'diffuse');
-intens3.addColor(intensities3,'specular');
-const ax3 = light3.addFolder('axis');
-ax3.add(aixs3,'x');
-ax3.add(aixs3,'y');
-ax3.add(aixs3,'z');
-ax3.add(aixs3,'aperture',0,100);
-ax3.add(aixs3,'cutoff',0,100);
-
-//material
-materialFolder.addColor(material,'Ka');
-materialFolder.addColor(material,'Kd');
-materialFolder.addColor(material,'Ks');
-materialFolder.add(material,'shininess');
-
-
+import * as STACK from '../../libs/stack.js';
 
 function main(shaders){
+    
     let camera = {
         eye: vec3(0,5,10),
         at: vec3(0,0,0),
@@ -133,7 +27,7 @@ function main(shaders){
             ambient: [50, 50, 50],
             diffuse: [60,60,60],
             specular: [200,200,200],
-            position: [0.0,0.0,10.0,1.0],
+            position: [0,0.0,10.0,1.0],
             axis: [0.0,0.0,-1.0],
             aperture: 10.0,
             cutoff:10
@@ -148,13 +42,13 @@ function main(shaders){
             cutoff: -1,
         },
         {
-        ambient: [75, 75, 100],
-        diffuse: [75, 75, 100],
-        specular: [150, 150, 175],
-        position: [5.0, 5.0, 2.0, 1.0],
-        axis: [-5.0, -5.0, -2.0],
-        aperture: 180.0,
-        cutoff: -1,
+            ambient: [75, 75, 100],
+            diffuse: [75, 75, 100],
+            specular: [150, 150, 175],
+            position: [5.0, 5.0, 2.0, 1.0],
+            axis: [-5.0, -5.0, -2.0],
+            aperture: 180.0,
+            cutoff: -1,
         }
     ];
 
@@ -165,28 +59,28 @@ function main(shaders){
         shininess: 1.0
     };
 
-    let grayMaterial = {
+    let bunnyMaterial = {
         Ka: [150, 150, 150],
         Kd: [125, 125, 125],
         Ks: [200,200,200],
         shininess: 100.0
     }
 
-    let redMaterial = {
+    let cubeMaterial = {
         Ka: [150, 75, 75],
         Kd: [150, 75, 75],
         Ks: [200,200,200],
         shininess: 100.0
     }
 
-    let greenMaterial = {
+    let torusMaterial = {
         Ka: [50, 150, 50],
         Kd: [50, 150, 50],
         Ks: [200,200,200],
         shininess: 100.0
     }
 
-    let blueMaterial = {
+    let cylinderMaterial = {
         Ka: [0, 150, 100],
         Kd: [50, 125, 100],
         Ks: [200,200,200],
@@ -195,8 +89,8 @@ function main(shaders){
 
     //Visualization options
     let options = {
-        cullFace: true,
-        depthTest: true
+        'backface culling': true,
+        'depth test': true
     };
 
     //Generate the canvas to fill the entire Window
@@ -211,31 +105,189 @@ function main(shaders){
 
     const program = programP;
 
+    const gui = new dat.GUI();
+    const optionsFolder = gui.addFolder('options');
+    const cameraFolder = gui.addFolder('camera');
+    const lightsFolder = gui.addFolder('lights');
+    const materialFolder = gui.addFolder('material');
+
+    //options
+    optionsFolder.add(options, 'backface culling',false,true);
+    optionsFolder.add(options, 'depth test',0,100);
+
+    //camera
+    cameraFolder.add(camera, 'fovy', 0, 360);
+    cameraFolder.add(camera, 'near' , 0, 360);
+    cameraFolder.add(camera, 'far' , 0, 360);
+    const eyeFolder = cameraFolder.addFolder('eye');
+    const atFolder= cameraFolder.addFolder('at');
+    const upFolder= cameraFolder.addFolder('up');
+    eyeFolder.add(camera.eye, 0).name('x');
+    eyeFolder.add(camera.eye, 1).name('y');
+    eyeFolder.add(camera.eye, 2).name('z');
+    //at
+    atFolder.add(camera.at, 0).name('x');
+    atFolder.add(camera.at, 1).name('y');
+    atFolder.add(camera.at, 2).name('z');
+    //up
+    upFolder.add(camera.up, 0).name('x');
+    upFolder.add(camera.up, 1).name('y');
+    upFolder.add(camera.up, 2).name('z');
+
+    //lights
+    //Light1
+    const light1 = lightsFolder.addFolder('Light1');
+    const pos1 = light1.addFolder('position');
+    pos1.add(lights[0].position,0).name('x');
+    pos1.add(lights[0].position,1).name('y');
+    pos1.add(lights[0].position,2).name('z');
+    pos1.add(lights[0].position,3).name('w');
+    const intens1 = light1.addFolder('intensities');
+    intens1.addColor(lights[0], "ambient");
+    intens1.addColor(lights[0], "diffuse");
+    intens1.addColor(lights[0], "specular");
+    const ax1 = light1.addFolder('axis');
+    ax1.add(lights[0].axis,0).name('x');
+    ax1.add(lights[0].axis,1).name('y');
+    ax1.add(lights[0].axis,2).name('z');
+    light1.add(lights[0], 'aperture',0,100);
+    light1.add(lights[0],'cutoff',0,100);
+
+    //Light2
+    const light2 = lightsFolder.addFolder('Light2');
+    const pos2 = light2.addFolder('position');
+    pos2.add(lights[1].position,0).name('x');
+    pos2.add(lights[1].position,1).name('y');
+    pos2.add(lights[1].position,2).name('z');
+    pos2.add(lights[1].position,3).name('w');
+    const intens2 = light2.addFolder('intensities');
+    intens2.addColor(lights[1], "ambient");
+    intens2.addColor(lights[1], "diffuse");
+    intens2.addColor(lights[1], "specular");
+    const ax2 = light2.addFolder('axis');
+    ax2.add(lights[1].axis,0).name('x');
+    ax2.add(lights[1].axis,1).name('y');
+    ax2.add(lights[1].axis,2).name('z');
+    light2.add(lights[1], 'aperture',0,100);
+    light2.add(lights[1],'cutoff',0,100);
+
+    //Light3
+    const light3 = lightsFolder.addFolder('Light3');
+    const pos3 = light3.addFolder('position');
+    pos3.add(lights[2].position,0).name('x');
+    pos3.add(lights[2].position,1).name('y');
+    pos3.add(lights[2].position,2).name('z');
+    pos3.add(lights[2].position,3).name('w');
+    const intens3 = light3.addFolder('intensities');
+    intens3.addColor(lights[2], "ambient");
+    intens3.addColor(lights[2], "diffuse");
+    intens3.addColor(lights[2], "specular");
+    const ax3 = light3.addFolder('axis');
+    ax3.add(lights[2].axis,0).name('x');
+    ax3.add(lights[2].axis,1).name('y');
+    ax3.add(lights[2].axis,2).name('z');
+    light3.add(lights[2], 'aperture',0,100);
+    light3.add(lights[2],'cutoff',0,100);
+
+    //material
+    materialFolder.addColor(bunnyMaterial, 'Ka');
+    materialFolder.addColor(bunnyMaterial,'Kd');
+    materialFolder.addColor(bunnyMaterial,'Ks');
+    materialFolder.add(bunnyMaterial,'shininess');
+
+
+
     gl.viewport(0,0,canvas.width, canvas.height);
-    gl.clearColor(0, 0, 0, 1.0); //cor de fundo
+    gl.clearColor(1, 1, 1, 1.0); //cor de fundo
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-    gl.enable(gl.BACK);
+    //gl.enable(gl.BACK);
+
+    resizeCanvasToFullWindow();
+    window.addEventListener('resize', resizeCanvasToFullWindow);
 
     CUBE.init(gl);
     SPHERE.init(gl);
     TORUS.init(gl);
     CYLINDER.init(gl);
     BUNNY.init(gl);
+    
 
-    setupGUI();
+    window.requestAnimationFrame(render);
+
+    function resizeCanvasToFullWindow()
+    {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        camera.aspect = canvas.width / canvas.height;
+
+        gl.viewport(0,0,canvas.width, canvas.height);
+    }
 
     let mode = gl.TRIANGLES;
 
     let mView = lookAt( camera.eye, camera.at, camera.up);
-    let mProjection = perspective(camera.fovy, __, camera.near, camera.far); //INCOMPLETO
-
-    
-
-
+    let mProjection = perspective(camera.fovy, aspect, camera.near, camera.far);
     
     
+    let lastX, lastY;
+    let down = false;
+
+    function render()
+    {
+        window.requestAnimationFrame(render);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        
+        gl.useProgram(program);
+        
+        STACK.loadMatrix(mView);
+    
+
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mModelView"), false, flatten(STACK.modelView()));
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mProjection"), false, flatten(mProjection));
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mNormals"), false, flatten(normalMatrix(STACK.modelView())));
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mView"), false, flatten(mView));
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mViewNormals"), false, flatten(normalMatrix(mView)));
+        gl.uniform1i(gl.getUniformLocation(program,"uNLights"),lights.length);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mViewF"), false, flatten(mView));
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mViewNormalsF"), false, flatten(normalMatrix(mView)));
+
+        
+        gl.uniform3fv(gl.getUniformLocation(program,"T.Ka"),groundMaterial.Ka );
+        gl.uniform3fv(gl.getUniformLocation(program,"uMaterial.Kd"),groundMaterial.Kd );
+        gl.uniform3fv(gl.getUniformLocation(program,"uMaterial.Ks"),groundMaterial.Ks );
+        gl.uniform1f(gl.getUniformLocation(program,"uMaterial.shininess"),groundMaterial.shininess);
+
+        STACK.pushMatrix();
+        STACK.multTranslation([0,-0.25,0])
+        STACK.multScale([10,0.5,10]);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "mModelView"), false, flatten(STACK.modelView()));
+        CUBE.draw(gl, program, mode);
+        STACK.popMatrix();
+        for(let i = 0; i < lights.length; i++){
+            let ambient = vec3(lights[i].ambient[0] / 255 , lights[i].ambient[1] / 255 ,lights[i].ambient[2]/255);
+            let diffuse = vec3(lights[i].diffuse[0] / 255 , lights[i].diffuse[1] / 255 ,lights[i].diffuse[2]/255);
+            let specular = vec3(lights[i].specular[0] / 255 , lights[i].specular[1] / 255 ,lights[i].specular[2]/255);
+        
+                
+            gl.uniform3fv(gl.getUniformLocation(program, "uLight[" + i +"].Ia"),ambient);
+            gl.uniform3fv(gl.getUniformLocation(program, "uLight[" + i +"].Id"),diffuse);
+            gl.uniform3fv(gl.getUniformLocation(program, "uLight[" + i +"].Is"),specular);
+            gl.uniform4fv(gl.getUniformLocation(program, "uLight[" + i +"].pos"),lights[i].position);
+            gl.uniform1i(gl.getUniformLocation(program, "uLight[" + i +"].isDirectional"), lights[i].directional);
+            gl.uniform1i(gl.getUniformLocation(program, "uLight[" + i +"].isActive"), lights[i].active);
+    
+        }
 
 
-
+        
+    }
+    
 }
+
+
+const urls = ['shader.vert', 'shader.frag'];
+
+loadShadersFromURLS(urls).then( shaders => main(shaders));
